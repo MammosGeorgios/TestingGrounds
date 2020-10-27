4d68727e7b415f3afee0d5dc054e3286ea47c793
MammosGeorgios <mammosgeorgios@gmail.com>
2020-10-27 14:42:13 +0200

    Added RegressionFactorial example
    Just a small example

---
+++ RegressionFactorial/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegressionFactorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 4;
+            Console.WriteLine("{0}! = {1}", number, Factorial(number));
+
+
+        }
+
+        static int Factorial (int n)
+        {
+            if (n > 1) return n * Factorial(n - 1);
+            else return 1;
+        }
+    }
+}
