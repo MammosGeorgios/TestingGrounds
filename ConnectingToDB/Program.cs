3367d7a9be39e1bf6665200508fb1395c1dc0ac2
MammosGeorgios <mammosgeorgios@gmail.com>
2020-11-20 13:24:19 +0200

    update ConnectingToDB example

--- ConnectingToDB/Program.cs
+++ ConnectingToDB/Program.cs
@@ -12,25 +12,93 @@ namespace ConnectingToDB
     {
         static void Main(string[] args)
         {
+            //SelectCustomers();
 
+            //InsertCategory();
+
+            //DeleteCategory();
+
+            Procedure_CustOrderHist();
+
+        }
+
+        private static void Procedure_CustOrderHist()
+        {
+            using (SqlConnection conn = new SqlConnection("Server =.; Database = Northwind; Trusted_Connection = True;"))
+            {
+
+                conn.Open();
+                //string deleteString = "DELETE FROM Categories WHERE CategoryName = 'Miscellaneous'";
+                using (SqlCommand cmd = new SqlCommand("CustOrderHist", conn))
+                {
+
+                    cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                    string custID = "CACTU";
+                    cmd.Parameters.Add(new SqlParameter("@CustomerID", custID));
+
+                    using (SqlDataReader rdr = cmd.ExecuteReader())
+                    {
+
+                        if (rdr != null)
+                        {
+                            while (rdr.Read())
+                            {
+                                Console.WriteLine("\t{0,-30}\t{1}", rdr[0], rdr[1]);
+                            }
+                        }
+                    }
+
+
+                }
+            }
+        }
+
+        private static void DeleteCategory()
+        {
+            using (SqlConnection conn = new SqlConnection("Server =.; Database = Northwind; Trusted_Connection = True;")){
+                
+                conn.Open();
+                string deleteString = "DELETE FROM Categories WHERE CategoryName = 'Miscellaneous'";
+                using (SqlCommand cmd = new SqlCommand(deleteString, conn))
+                {
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        private static void InsertCategory()
+        {
+            SqlConnection conn = new SqlConnection("Server =.; Database = Northwind; Trusted_Connection = True;");
+            
+            conn.Open();
+            string insertString = "INSERT INTO Categories (CategoryName, Description) VALUES ('Miscellaneous', 'Whatever, it doesnt matter')";
+            SqlCommand cmd = new SqlCommand(insertString, conn);
+            cmd.ExecuteNonQuery();
+        }
+
+        private static void SelectCustomers()
+        {
             SqlConnection conn = new SqlConnection("Server =.; Database = Northwind; Trusted_Connection = True;");
             SqlDataReader rdr = null;
 
             try
             {
                 conn.Open();
-                SqlCommand cmd = new SqlCommand("SELECT * FROM Customers",conn);
+                SqlCommand cmd = new SqlCommand("SELECT * FROM Customers", conn);
                 rdr = cmd.ExecuteReader();
                 if (rdr != null)
                 {
                     while (rdr.Read())
                     {
-                        Console.WriteLine("\t{0}\t{1}\t{2}",rdr[0],rdr[1],rdr[2]);
+                        Console.WriteLine("\t{0}\t{1}\t{2}", rdr[0], rdr[1], rdr[2]);
                     }
                 }
 
+
+
+                
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 Console.WriteLine(ex.Message);
             }
@@ -39,8 +107,6 @@ namespace ConnectingToDB
                 if (rdr != null) rdr.Close();
                 if (conn != null) conn.Close();
             }
-            
-
         }
     }
 }
