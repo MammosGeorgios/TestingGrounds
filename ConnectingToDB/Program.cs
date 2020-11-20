645229c7509a086a32a8628c736cb9a6f829d3b1
MammosGeorgios <mammosgeorgios@gmail.com>
2020-11-20 11:24:08 +0200

    ConnectingToDB added

---
+++ ConnectingToDB/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ConnectingToDB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            SqlConnection conn = new SqlConnection("Server =.; Database = Northwind; Trusted_Connection = True;");
+            SqlDataReader rdr = null;
+
+            try
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand("SELECT * FROM Customers",conn);
+                rdr = cmd.ExecuteReader();
+                if (rdr != null)
+                {
+                    while (rdr.Read())
+                    {
+                        Console.WriteLine("\t{0}\t{1}\t{2}",rdr[0],rdr[1],rdr[2]);
+                    }
+                }
+
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                if (rdr != null) rdr.Close();
+                if (conn != null) conn.Close();
+            }
+            
+
+        }
+    }
+}
---
+++ SimpleTests/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleTests
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            int i = 0;
+            Console.WriteLine( i);
+
+            decimal d = 100.32423423m;
+
+            double d2 = 101024.32423;
+        }
+    }
+}
